03aedac72ffe59077d38d1574a56ffabe699b692
James <boydston.j@gmail.com>
2017-07-19 13:38:19 -0700

    still working search

--- main.js
+++ main.js
@@ -13,46 +13,56 @@ let recipe = " "
 let ingred = " "
 let url = site + "?i=" + ingred + "&q=" + recipe
 
-fetch(url)
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log(response.status);
-        return;
+function doFetch() {
+  fetch(url)
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log(response.status);
+          return;
+        }
+        response.json().then(function(data) {
+          console.log(`Here is the data: ${data.title}`, data);
+          let titleStr = `${data.title} Search`
+          title.innerHTML = titleStr
+
+          // http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3
+
+          let headingStr = ``
+          headingStr += `<h1>${data.title} Search</h1>`
+          headingStr += `<form>Recipe Type:<input id='recipe' type="text" name="recipe" value="" maxlength="100" /><br />`
+          headingStr += `Ingredients:<input id='ingred' type="text" name="ingredients" value="" maxlength="100" /><br />`
+          headingStr += `<input id='search' type="submit" value="Search" /></form>`
+          headingStr += `<p>Results from <a href="${data.href}">${data.href}</a></p>`
+          headingStr += `<hr>`
+          heading.innerHTML = headingStr
+
+            let mainStr = ``
+          data.results.map(function(item) {
+
+              mainStr += `<div class="boxes">
+              <img class="profilePic" src="${item.thumbnail}">
+              <p class="rName"><a href="${item.href}">${item.title}</a></p>
+              <ul>${item.ingredients}</ul>
+              </div>`
+              main.innerHTML = mainStr
+          })
+
+
+          let footingStr = `<hr><footer>${data.title} Version ${data.version} <a href="${data.href}">${data.href}</a></footer>`
+          footing.innerHTML = footingStr
+        });
       }
-      response.json().then(function(data) {
-        console.log(`Here is the data: ${data.title}`, data);
-        let titleStr = `${data.title} Search`
-        title.innerHTML = titleStr
+    )
+    .catch(function(err) {
+      console.log("Fetch Error :-S", err);
+    });
+}
 
-        // http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3
+doFetch()
 
-        let headingStr = ``
-        headingStr += `<h1>${data.title} Search</h1>`
-        headingStr += `<form>Recipe Type:<input id='recipe' type="text" name="recipe" value="" maxlength="100" /><br />`
-        headingStr += `Ingredients:<input id='ingred' type="text" name="ingredients" value="" maxlength="100" /><br />`
-        headingStr += `<input id='search' type="submit" value="Search" /></form>`
-        headingStr += `<p>Results from <a href="${data.href}">${data.href}</a></p>`
-        headingStr += `<hr>`
-        heading.innerHTML = headingStr
-
-          let mainStr = ``
-        data.results.map(function(item) {
-
-            mainStr += `<div class="boxes">
-            <img class="profilePic" src="${item.thumbnail}">
-            <p class="rName"><a href="${item.href}">${item.title}</a></p>
-            <ul>${item.ingredients}</ul>
-            </div>`
-            main.innerHTML = mainStr
-        })
-
-
-        let footingStr = `<hr><footer>${data.title} Version ${data.version} <a href="${data.href}">${data.href}</a></footer>`
-        footing.innerHTML = footingStr
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log("Fetch Error :-S", err);
-  });
+search.addEventListener('click', function(event) {
+  recipe = recipeI.value
+  ingred = ingredI.value
+  console.log(url);
+})
